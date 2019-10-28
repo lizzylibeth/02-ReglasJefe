e70b48301d6dc51b1bc2823cc66dfa2ab7d52815
eli <eli@192.168.1.134>
2019-10-28 17:31:08 +0100

    :tada: Commit inicial

--- src/org/japo/java/main/Main.java
+++ src/org/japo/java/main/Main.java
@@ -20,28 +20,25 @@ package org.japo.java.main;
  * @author Elísabet Palomino
  */
 public class Main {
-    
+
     public static void main(String[] args) {            //System es una clase,out es un recurso de la clase System. el punto indica el recurso. 
-        
+
         //Manera 1
-        
-        System.out.println ("ARTÍCULO 1. El jefe tiene la razón.");
-        System.out.println ("ARTÍCULO 2. El jefe SIEMPRE tiene la razón."); 
-        System.out.println ("ARTÍCULO 3. En caso de duda se aplicará el artículo 2."); 
-           
+        System.out.println("ARTÍCULO 1. El jefe tiene la razón.");
+        System.out.println("ARTÍCULO 2. El jefe SIEMPRE tiene la razón.");
+        System.out.println("ARTÍCULO 3. En caso de duda se aplicará el artículo 2.");
+
         //Manera 2     
-        
         System.out.println(" ");
-        
-        final String REGLA1="ARTÍCULO %d.";
-        final String REGLA2="El jefe %s tiene la razón.";
-        final String REGLA3="En caso de duda se aplicará el artículo 2.";
-        
-        System.out.printf(REGLA1 + REGLA2 + "%n",1,"");
-        System.out.printf(REGLA1 + REGLA2 + "%n",2,"SIEMPRE");
-        System.out.printf(REGLA1 + REGLA3 + "%n",3);
-        
-      
+
+        final String REGLA1 = "ARTÍCULO %d.";
+        final String REGLA2 = "El jefe %s tiene la razón.";
+        final String REGLA3 = "En caso de duda se aplicará el artículo 2.";
+
+        System.out.printf(REGLA1 + REGLA2 + "%n", 1, "");
+        System.out.printf(REGLA1 + REGLA2 + "%n", 2, "SIEMPRE");
+        System.out.printf(REGLA1 + REGLA3 + "%n", 3);
+
     }//main
-    
+
 }//class
